afe64f37532eef874364f4db624fb7d4f07d51d1
Jai Tiwari <contactjaitiwari@gmail.com>
2017-04-09 00:50:58 -0400

    final

--- server.js
+++ server.js
@@ -5,29 +5,66 @@ var path = require('path');
 require('dotenv').config();
 
 var app = express();
+var url = process.env.MONGOLAB_URI;
+var MongoClient = mongodb.MongoClient;
+var db;
+var coll;
+var latest_seq = 0;
+
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening to " + process.env.PORT || 3000);
+
+MongoClient.connect(url, (err, database) => {
+  if (err) { throw err; }
+  db = database;
+  coll = db.collection('urlmappings');
+  app.listen(process.env.PORT || 3000, () => console.log("Listening to " + process.env.PORT || 3000));
+
 });
 
-//We need to work with "MongoClient" interface in order to connect to a mongodb server.
-var MongoClient = mongodb.MongoClient;
+app.get('/new/*', (req, res) => {
 
-// Connection URL. This is where your mongodb server is running.
-
-//(Focus on This Variable)
-var url = process.env.MONGOLAB_URI;
-//(Focus on This Variable)
-
-// Use connect method to connect to the Server
-MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    console.log('Connection established to', url);
-    // do some work here with the database.
-
-    //Close connection
-    db.close();
+  if (!(/^(http){1}s{0,1}:{1}(\/\/){1}(www.){1}/.test(req.params[0]))) {
+    res.send(JSON.stringify({
+      "error": "Improper format. Please use (https://www.) or (http://www.)"
+    }));
+    return 0;
   }
+
+  var save_object = {};
+  coll.ensureIndex({ "sequence": 1 })
+  console.log("New request received to store : " + req.params[0]);
+  coll.find().sort({ "sequence": -1 }).limit(1).toArray((err, results) => {
+    if (results.length !== 0) {
+      latest_seq = results[0]["sequence"] + 1;
+    } else {
+      latest_seq = 0;
+    }
+    save_object.original_url = req.params[0];
+    save_object.sequence = latest_seq;
+    save_object.short_url = req.get('host') + '/' + latest_seq;
+    coll.insert(save_object);
+    res.send(JSON.stringify({
+      original_url: save_object.original_url,
+      short_url: save_object.short_url
+    }));
+  });
+});
+
+app.get('/:sequence', (req, res) => {
+  console.log("Unshort request received for sequence " + req.params.sequence);
+  console.log(typeof req.params.sequence, req.params.sequence);
+  coll.find({
+    "sequence": parseInt(req.params.sequence)
+  }).toArray((err, results) => {
+    console.log(results);
+    if (results.length !== 0) {
+      console.log("Redirecting to : " + results[0]['original_url']);
+      res.redirect(results[0]['original_url']);
+      res.end();
+    } else {
+      res.send(JSON.stringify({
+        "error": "This url is not on the database."
+      }));
+    }
+  });
 });
